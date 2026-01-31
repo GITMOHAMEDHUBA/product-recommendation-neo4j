cb8a877517b4ff340323ef26d6e7ab397e2fa986
GITMOHAMEDHUBA <medt6500@gmail.com>
2026-01-31 13:26:57 +0100

    Add Neo4j query to build product associations

--- scripts/build_associations.py
+++ scripts/build_associations.py
@@ -1,1 +1,19 @@
+from neo4j import GraphDatabase
+
+driver = GraphDatabase.driver(
+    "bolt://localhost:7687",
+    auth=("neo4j", "password")
+)
+
+query = """
+MATCH (o:Order)-[:CONTAINS]->(p1:Product)
+MATCH (o)-[:CONTAINS]->(p2:Product)
+WHERE p1 <> p2
+WITH p1, p2, count(*) AS freq
+MERGE (p1)-[r:BOUGHT_WITH]->(p2)
+SET r.count = freq
+"""
+
+with driver.session() as session:
+    session.run(query)
 
